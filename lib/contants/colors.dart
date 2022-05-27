888aa56b37f95f4e10a63762b1bc6e4d14311571
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-05-27 16:57:23 +0100

    onboarding set

---
+++ lib/contants/colors.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const base = Color(0xFFB34666);
+const white = Color(0xFFffffff);
--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../contants/colors.dart';
+
 class OnBoarding extends StatelessWidget {
   const OnBoarding({Key? key}) : super(key: key);
 
@@ -7,36 +9,75 @@ class OnBoarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration:  BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("images/bg.png"),
-          fit: BoxFit.fitWidth,
-          alignment: Alignment.center
-          ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/bg.png"),
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.center),
         ),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-
-              children:  <Widget>[
-                SizedBox(
-                  height: 100,
-                ),
-                Image(
-                  image: AssetImage('images/logo.png'),
-                  width: 100,
-
-                ),
-                Image(
-                  image: AssetImage('images/logo.png'),
-                  width: 80,
-                ),
-
-              ],
-
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 170,
+                  ),
+                  Image(
+                    image: AssetImage('images/logo.png'),
+                    width: 100,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print('hello');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              color: base,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Sign In',
+                                style: TextStyle(color: white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Container(
+                            height: 40,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              color: base,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(color: white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 60,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
